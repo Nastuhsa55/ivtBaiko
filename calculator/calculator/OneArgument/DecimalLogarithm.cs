7c3bae85e30197b8a4156d55990b64b46cdd013e
alina1307 <alina.beskempirova@mail.ru>
2017-07-06 00:18:24 +0600

    added decimal logarithm

---
+++ calculator/calculator/OneArgument/DecimalLogarithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.OneArgument
+{
+    class DecimalLogarithm : ICanculator2
+    {
+        public double Calculate(double first)
+        {
+            return Math.Log10(first);
+        }
+    }
+}
