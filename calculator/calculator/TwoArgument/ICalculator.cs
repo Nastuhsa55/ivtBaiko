38466e61587bc64c8b6d1886e671e4ae80a990ea
Nastuhsa55 <nastia612284@gmail.com>
2017-07-05 14:12:00 +0600

    refactoring

--- calculator/calculator/TwoArgument/ICalculator.cs
+++ calculator/calculator/TwoArgument/ICalculator.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace calculator
+﻿namespace calculator
 {
-  public  interface ICalculator
+    public  interface ICalculator
     {
         double Calculate(double first, double second);
 
