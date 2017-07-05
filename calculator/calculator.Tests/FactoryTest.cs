d0ac0372f56e4f2c435661da5addf3a71a702fc3
Nastuhsa55 <nastia612284@gmail.com>
2017-07-05 15:53:37 +0600

    Test

--- calculator/calculator.Tests/FactoryTest.cs
+++ calculator/calculator.Tests/FactoryTest.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace calculator.Tests
 {
     class FactoryTest
     {
+        [TestCase("plus", typeof(Addition))]
+        [TestCase("minus", typeof(Subtraction))]
+        [TestCase("multiplication", typeof(Multiplication))]
+        [TestCase("division", typeof(Division))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = CalculatorFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
     }
 }
