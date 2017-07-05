6032f91c041d62cd1a78d892fffd88b37d4bdc17
alina1307 <alina.beskempirova@mail.ru>
2017-07-06 00:16:14 +0600

    added module of x

---
+++ calculator/calculator/OneArgument/ModuleOfX.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculator.OneArgument
+{
+    class ModuleOfX : ICanculator2
+    {
+        public double Calculate(double first)
+        {
+            return Math.Abs(first);
+        }
+    }
+}
